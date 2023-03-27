3222030e9cc63242cc93934a2388b712509785b5
Francisco Breno <fbreno.dev@gmail.com>
2023-03-27 14:23:58 -0300

    Atividade 01 - Up

--- atividades/src/App.js
+++ atividades/src/App.js
@@ -1,26 +1,17 @@
 // import MeusDadosV1 from "./components/atividade00/01MeusDados";
-import MeusDadosV2 from "./components/atividade00/02MeusDados";
-import Temperatura from "./components/atividade00/03Temperatura"
+// import MeusDadosV2 from "./components/atividade00/02MeusDados";
+// import Temperatura from "./components/atividade00/03Temperatura";
+
+// import Pai from './components/atividade01/questao01/01Pai';
+// import MeuPc from "./components/atividade01/02MeuPc";
+import { World, Arena } from './components/atividade01/03Batalha';
 
 function App() {
   return (
     <div className="App">
-      <div>
-        {/* <MeusDadosV1 /> */}
-        <MeusDadosV2 
-          nome="Francisco Breno da Silveira"
-          curso="Ciência da Computação"
-          universidade="Universidade Federal do Ceará (UFC) - Campus Quixadá"
-        />
-      </div>
-      <div>
-        <Temperatura 
-          Tcelsius={21}
-          Tfahrenheit={21}
-          Tkelvin={21}
-        />
-      <iframe src="https://giphy.com/embed/zZC2AqB84z7zFnlkbF" title="Anya gif" className="giphy-embed" allowFullScreen></iframe>
-      </div>
+      <World nome="Rinha de animes">
+        <Arena />
+      </World>
     </div>
   );
 }
