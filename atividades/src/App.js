6f3f8919676867b244f84ab005f3f04bc67b6ad2
Francisco Breno <fbreno.dev@gmail.com>
2023-04-03 11:28:43 -0300

    Atividade 02 - up

--- atividades/src/App.js
+++ atividades/src/App.js
@@ -4,14 +4,28 @@
 
 // import Pai from './components/atividade01/questao01/01Pai';
 // import MeuPc from "./components/atividade01/02MeuPc";
-import { World, Arena } from './components/atividade01/03Batalha';
+// import { World, Arena } from './components/atividade01/03Batalha';
+
+import FuncaoA from './components/atividade02/Q01/FuncaoA';
+import ComponenteAvo from './components/atividade02/Q02/ComponenteAvo';
+import ComponenteAvoV2 from './components/atividade02/Q03/ComponenteAvoV2';
+
 
 function App() {
   return (
     <div className="App">
-      <World nome="Rinha de animes">
-        <Arena />
-      </World>
+      <div>
+        <h2>Questão 01</h2>
+        <FuncaoA />
+      </div>
+      <div>
+        <h2>Questão 02</h2>
+        <ComponenteAvo />
+      </div>
+      <div>
+        <h2>Questão 03</h2>
+          <ComponenteAvoV2 />
+      </div>
     </div>
   );
 }
