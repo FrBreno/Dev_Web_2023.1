a70ca318e5f70c5d78d8ec492b24897cea6a8df9
Francisco Breno <fbreno.dev@gmail.com>
2023-04-05 09:43:01 -0300

    Aula 05 - Up

--- aulas/src/App.js
+++ aulas/src/App.js
@@ -2,18 +2,19 @@
 // import Calculadora from './components/01Calculadora';
 // import MyProps from './components/02MyProps';
 // import FunctionA from './components/03 Hierarquia/FunctionA';
-
 // import { Header, Body, Footer } from './components/04Multiplo';
 // import * as Site from './components/04Multiplo';
-
 // import {Supermercado, Legume, Bebidas} from './components/05Children';
+// import FuncaoA from './components/06 Contexto/FuncaoA';
+// import { SaveData, LoadData } from './components/07 Storage/MyStorage';
+// import Pai from './components/08 PaiFilho/Pai';
 
-import FuncaoA from './components/06 Contexto/FuncaoA';
+import Contador from "./components/09 Estados/Contador";
 
 function App() {
   return (
     <div className='App'>
-      <FuncaoA />
+      <Contador />
     </div>
   );
 }
--- aulas/src/components/06 Contexto/MeuContexto.jsx
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react';
-
-const MinhaCor = createContext();
-
-export default MinhaCor;
