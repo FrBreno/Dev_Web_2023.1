f907159a8124bc2ec0a942a566e8349f5cd1543c
Francisco Breno <fbreno.dev@gmail.com>
2023-05-29 11:34:25 -0300

    Aula 09 - MUI_02

--- aulas/src/components/13 MUI/02 Crud/professor/Cadastrar.jsx
+++ aulas/src/components/13 MUI/02 Crud/professor/Cadastrar.jsx
@@ -1,13 +1,24 @@
-import { Box, Button, TextField, Typography } from "@mui/material";
+import { Box, Button, Checkbox, FormControl, FormControlLabel, FormGroup, FormLabel, InputLabel, MenuItem, Select, TextField, Typography } from "@mui/material";
 import { useState } from "react";
 
 const CadastrarProfessor = () => {
     const [nome, setNome] = useState('');
     const [curso, setCurso] = useState('');
-    
+    const [titulacao, setTitulacao] = useState('GRAD');
+    const [ai, setAi] = useState({cg:false, mc:false,al:false,es:false});
+
+    const {cg, mc, al, es} = ai;
+
     function handleSubmit(event) {
         event.preventDefault()
         console.log(`${nome} - ${curso}`)
+    }
+
+    function handleCheckbox(event) {
+        setAi({
+            ...ai,
+            [event.target.name]: event.target.checked,
+        })
     }
 
     return (
@@ -39,6 +50,27 @@ const CadastrarProfessor = () => {
                     name="curso"
                     onChange={(event) => setCurso(event.target.value)}
                 />
+                <FormControl sx={{mt:2, width:'100%'}} required>
+                    <InputLabel id="select-tit-label">Titulação</InputLabel>
+                    <Select
+                        labelId="select-tit-label"
+                        label="Titulação"
+                        value={titulacao}
+                    >
+                        <MenuItem value="GRAD">Graduação</MenuItem>
+                        <MenuItem value="MEST">Mestrado</MenuItem>
+                        <MenuItem value="DOUT">Doutorado</MenuItem>
+                    </Select>
+                </FormControl>
+                <FormControl sx={{mt:2, ml:2, width:'100%'}} component="fieldset" variant="standard">
+                    <FormLabel component="legend" sx={{fontSize:12, mb:2}}>Áreas de Interesse</FormLabel>
+                    <FormGroup>
+                        <FormControlLabel control={<Checkbox onChange={handleCheckbox} checked={cg} name="cg" />} label="Computação Gráfica" />
+                        <FormControlLabel control={<Checkbox onChange={handleCheckbox} checked={mc} name="mc" />} label="Matemática Computacional" />
+                        <FormControlLabel control={<Checkbox onChange={handleCheckbox} checked={al} name="al" />} label="Algoritmos" />
+                        <FormControlLabel control={<Checkbox onChange={handleCheckbox} checked={es} name="es" />} label="Engenharia de Software" />
+                    </FormGroup>
+                </FormControl>
                 <Box sx={{display: "flex", justifyContent: "center", mt: 2}}>
                     <Button type="submit" variant="contained">
                         Cadastrar
--- aulas/src/components/13 MUI/02 Crud/professor/Listar.jsx
+++ aulas/src/components/13 MUI/02 Crud/professor/Listar.jsx
@@ -1,9 +1,81 @@
+import { Paper, Table, TableBody, TableContainer, TableHead, TableRow, Typography, Box } from '@mui/material';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import { styled } from '@mui/material/styles';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+
+
 const ListarProfessor = () => {
+    const professores = [
+        {id: 0, nome: 'Vito Corleone', curso: 'SI', titulacao: 'MEST'},
+        {id: 1, nome: 'Michael Corleone', curso: 'DD', titulacao: 'GRAD'},
+        {id: 2, nome: 'Luca Brasi', curso: 'SI', titulacao: 'MEST'},
+        {id: 3, nome: 'Kay Adams', curso: 'SI', titulacao: 'DOUT'},
+        {id: 4, nome: 'Peter Clemenza', curso: 'CC', titulacao: 'MEST'},
+    ];
+
     return (
         <>
-            <h2>Listar Professor</h2>
+            <Typography variant="h5" fontWeight="bold">Listar Professor</Typography>
+            <TableContainer component={Paper} sx={{mt:4, mb:4}}>
+                <Table sx={{minWidth:650}} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <StyledTableCell>ID</StyledTableCell>
+                            <StyledTableCell>NOME</StyledTableCell>
+                            <StyledTableCell>CURSO</StyledTableCell>
+                            <StyledTableCell>TITULAÇÃO</StyledTableCell>
+                            <StyledTableCell>AÇÕES</StyledTableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {
+                            professores.map(prof => {
+                                return (
+                                    <StyledTableRow>
+                                        <StyledTableCell>{prof.id}</StyledTableCell>
+                                        <StyledTableCell>{prof.nome}</StyledTableCell>
+                                        <StyledTableCell>{prof.curso}</StyledTableCell>
+                                        <StyledTableCell>{prof.titulacao}</StyledTableCell>
+                                        <StyledTableCell>
+                                            <Box>
+                                                <IconButton aria-label="edit" color="primary">
+                                                    <EditIcon />
+                                                </IconButton>
+                                                <IconButton aria-label="delte" color="error">
+                                                    <DeleteIcon />
+                                                </IconButton>
+                                            </Box>
+                                        </StyledTableCell>
+                                    </StyledTableRow>
+                                )
+                            })
+                        }
+                    </TableBody>
+                </Table>
+            </TableContainer>
         </>
     );
 }
 
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+    },
+  }));
+  
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+'&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+},
+// hide last border
+'&:last-child td, &:last-child th': {
+    border: 0,
+},
+}));
 export default ListarProfessor;
